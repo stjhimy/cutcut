51275766280d5556f13d33c622e0b1e17927c6c0
stjhimy <stjhimy@gmail.com>
2016-01-28 21:33:10 -0200

    Add copy_exif_from_original_file

--- lib/cutcut/media.rb
+++ lib/cutcut/media.rb
@@ -13,5 +13,13 @@ module CutCut
       system("ffmpeg -i #{@path} -movflags +faststart -vf scale=-2:1080 -c:v libx264 -crf 20 -preset ultrafast #{@output_file}") unless converted?
     end
 
+    def copy_exif_from_original_file
+      @output_exif = MiniExiftool.new(@output_file)
+      @output_exif.copy_tags_from(@path, "*")
+      @output_exif.date_time_original = original_date
+      @output_exif.create_date = original_date
+      @output_exif.modify_modify_date = original_date
+      @output_exif.save
+    end
   end
 end
