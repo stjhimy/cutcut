3709c8ea312a0e003c004bc9eb8c3e42c3885ddd
stjhimy <stjhimy@gmail.com>
2016-01-28 21:32:29 -0200

    Add convert method

--- lib/cutcut/media.rb
+++ lib/cutcut/media.rb
@@ -7,5 +7,11 @@ module CutCut
 
     def convert(options = {})
     end
+
+    def convert(options={})
+      @output_file = File.join(@output_path, @file_name + @file_extension)
+      system("ffmpeg -i #{@path} -movflags +faststart -vf scale=-2:1080 -c:v libx264 -crf 20 -preset ultrafast #{@output_file}") unless converted?
+    end
+
   end
 end
