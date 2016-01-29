f0f73670616479b51bba6650f2846b09177ab251
stjhimy <stjhimy@gmail.com>
2016-01-29 17:56:10 -0200

    Fix rubocop offenses

--- lib/cutcut/media.rb
+++ lib/cutcut/media.rb
@@ -6,16 +6,14 @@ module CutCut
     end
 
     def convert(options = {})
-    end
-
-    def convert(options={})
-      @output_file = File.join(@output_path, @file_name + @file_extension)
+      output_path = options[:output_path]
+      @output_file = File.join(output_path, @file_name + @file_extension)
       system("ffmpeg -i #{@path} -movflags +faststart -vf scale=-2:1080 -c:v libx264 -crf 20 -preset ultrafast #{@output_file}") unless converted?
     end
 
     def copy_exif_from_original_file
       @output_exif = MiniExiftool.new(@output_file)
-      @output_exif.copy_tags_from(@path, "*")
+      @output_exif.copy_tags_from(@path, '*')
       @output_exif.date_time_original = original_date
       @output_exif.create_date = original_date
       @output_exif.modify_modify_date = original_date
