0cc641775ee1b7fd6f39bf2205035f699bade6bd
stjhimy <stjhimy@gmail.com>
2016-02-23 10:06:06 -0300

    Update media_spec

--- spec/cutcut/media_spec.rb
+++ spec/cutcut/media_spec.rb
@@ -22,28 +22,34 @@ describe CutCut::Media do
     CutCut::Media.new(input_file: File.join(File.dirname(__FILE__), '../fixtures/example.MP4'))
   end
 
-  it 'initialize accessors' do
-    expect(media.input_file).to_not eq(nil)
-    expect(media.output_path).to_not eq(nil)
+  describe 'initialize' do
+    it 'initialize accessors' do
+      expect(media.input_file).to_not eq(nil)
+      expect(media.output_path).to_not eq(nil)
+    end
+
+    it 'return create_date tag' do
+      expect(media.original_date_time).to eq('2016-01-25 20:15:35 -0200')
+    end
   end
 
-  it 'convert media' do
-    expect(File.exist?(media.convert(scale: '1920:1080'))).to eq true
+  describe 'convert' do
+    it 'convert media' do
+      expect(File.exist?(media.convert(scale: '1920:1080'))).to eq true
+    end
+
+    it 'copy metadata' do
+      source = MiniExiftool.new(media.input_file)
+      target = MiniExiftool.new(media.convert(scale: '1920:1080', copy_metadata: true))
+      expect(source.create_date).to eq(target.create_date)
+    end
   end
 
-  it 'copy metadata' do
-    source = MiniExiftool.new(media.input_file)
-    target = MiniExiftool.new(media.convert(scale: '1920:1080', copy_metadata: true))
-    expect(source.create_date).to eq(target.create_date)
-  end
-
-  it 'return create_date tag' do
-    expect(media.original_date_time).to eq('2016-01-25 20:15:35 -0200')
-  end
-
-  it 'cut the video' do
-    media.cut(starts_at: '00:00', time: '0.5')
-    expect(File.exist?(File.join(File.dirname(__FILE__), '../fixtures/cut.mp4'))).to eq(true)
+  describe 'cut' do
+    it 'cut the video' do
+      media.cut(starts_at: '00:00', time: '0.5')
+      expect(File.exist?(File.join(File.dirname(__FILE__), '../fixtures/cut.mp4'))).to eq(true)
+    end
   end
 
   describe 'extract_screenshots' do
