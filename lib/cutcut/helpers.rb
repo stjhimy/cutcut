198e495c94a31915107cda54b3c79dad7c336038
stjhimy <stjhimy@gmail.com>
2016-03-11 11:18:05 -0300

    Fix typos on Helpers#longest_common_substring

--- lib/cutcut/helpers.rb
+++ lib/cutcut/helpers.rb
@@ -2,11 +2,11 @@ module CutCut
   # Helpers
   class Helpers
     def self.longest_common_substring(*strings)
-      shortest = strings.min_by(&:length)
-      maxlen = shortest.length
-      maxlen.downto(0) do |len|
-        0.upto(maxlen - len) do |start|
-          substr = shortest[start, len]
+      short = strings.min_by(&:length)
+      max = short.length
+      max.downto(0) do |len|
+        0.upto(max - len) do |start|
+          substr = short[start, len]
           return substr if strings.all? { |str| str.include? substr }
         end
       end
