13c6b97596a80a0b4041816fb2f820a32110cf13
stjhimy <stjhimy@gmail.com>
2016-03-17 15:45:55 -0300

    Fix rubucop offense

--- lib/cutcut/timelapse.rb
+++ lib/cutcut/timelapse.rb
@@ -21,16 +21,21 @@ module CutCut
       common_substring + "%#{start_number.chars.count}d.JPG"
     end
 
+    def original_date_time
+      exif = MiniExiftool.new(files.first)
+      exif.date_time_original || exif.create_date || exif.modify_date
+    end
+
     def convert(options = {})
       fps = options[:fps] || 30
-      execute_ffmpeg_command(
-        input: File.join(@input, input_basename),
-        output: @output,
-        raw_options: {
-          input: "-f image2  -start_number #{start_number} -framerate #{fps}",
-          output: '-c:v libx264 -r 30 -vf scale=-1:1080 -crf 23 -preset ultrafast -pix_fmt yuv420p'
-        }
-      )
+      execute_ffmpeg_command(input: File.join(@input, input_basename), output: @output,
+                             raw_options: {
+                               input: "-f image2  -start_number #{start_number} -framerate #{fps}",
+                               output: '-c:v libx264 -r 30 -vf scale=-1:1080 -crf 23 -preset ultrafast -pix_fmt yuv420p'
+                             }
+                            )
+
+      Helpers.copy_metadata(files.first, @output)
       @output
     end
   end
