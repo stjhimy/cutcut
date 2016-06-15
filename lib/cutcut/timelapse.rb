795d4f9987d2bf94411351ec60d7f1b4d0c562c2
stjhimy <stjhimy@gmail.com>
2016-06-15 16:18:00 -0300

    Fix offense

--- lib/cutcut/timelapse.rb
+++ lib/cutcut/timelapse.rb
@@ -28,12 +28,13 @@ module CutCut
 
     def convert(options = {})
       fps = options[:fps] || 30
-      execute_ffmpeg_command(input: File.join(@input, input_basename), output: @output,
-                             raw_options: {
-                               input: "-f image2  -start_number #{start_number} -framerate #{fps}",
-                               output: '-c:v libx264 -r 30 -vf scale=-1:1080 -crf 23 -preset ultrafast -pix_fmt yuv420p'
-                             }
-                            )
+      execute_ffmpeg_command(
+        input: File.join(@input, input_basename), output: @output,
+        raw_options: {
+          input: "-f image2  -start_number #{start_number} -framerate #{fps}",
+          output: '-c:v libx264 -r 30 -vf scale=-1:1080 -crf 23 -preset ultrafast -pix_fmt yuv420p'
+        }
+      )
 
       Helpers.copy_metadata(files.first, @output)
       @output
